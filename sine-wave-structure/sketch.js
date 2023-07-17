16fa0fc45675fccbbf0b4595735f77dfb8b0ae8e
nawzaysfinah <nawzaysfinah@gmail.com>
2023-07-17 12:03:13 +0800

    added rotation of form

--- sine-wave-structure/sketch.js
+++ sine-wave-structure/sketch.js
@@ -11,15 +11,19 @@ function draw() {
   noFill();
   stroke(255);
 
-  for (var i = 0; i < 20; i++) {
+  for (var i = 0; i < 50; i++) {
+    // determine colour of animation
     var r = map(sin(frameCount / 2), -1, 1, 100, 200);
     var g = map(i, 0, 50, 100, 200);
     var b = map(cos(frameCount), -1, 1, 200, 100);
 
     stroke(r, g, b);
 
+    rotate(frameCount / 500);
+
     beginShape();
-    for (var j = 0; j < 360; j += 10) {
+    for (var j = 0; j < 360; j += 30) {
+      // determine shape
       var rad = i * 3;
       var x = rad * cos(j);
       var y = rad * sin(j);
